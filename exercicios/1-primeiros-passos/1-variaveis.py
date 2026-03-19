769a0f9138b6bfdd6456b8fb9de077f16790bf36
ViviPFF <vivian_prestes_farias@yahoo.com.br>
2026-03-19 18:37:33 +0000

    Exercicio Variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,13 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Vivian Ferreira' \
+'profissão = Analista de Operações' \
+'objetivo = Aprender Python para melhorar no dia a dia'
 # Declare variáveis de cada tipo numérico
-
-
+idade = '37'
+salario = '5000' \
+'complexo = 2+1'
 # Declare uma variável do tipo booleano
+verdadeiro = 'True' \
+'falso = False'
